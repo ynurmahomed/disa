1e5fbef559f9ec3ca23ad8984018669cc3edc7c4
Yassin Nurmahomed <yassin.nurmahomed@fgh.org.mz>
2022-11-29 10:51:01 +0200

    DOI-71 Move to full java based configuration

--- omod/src/main/java/org/openmrs/module/disa/web/config/DisaWebConfig.java
+++ omod/src/main/java/org/openmrs/module/disa/web/config/DisaWebConfig.java
@@ -5,8 +5,10 @@ import org.springframework.aop.framework.ProxyFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableWebMvc
 public class DisaWebConfig {
 
     @Bean
