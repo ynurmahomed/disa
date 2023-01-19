7d6c6f010534f0ee51aad72176af16b022d2b289
Yassin Nurmahomed <yassin.nurmahomed@fgh.org.mz>
2023-01-19 16:41:49 +0200

    DOI-98 Move Disa Interoperabilidade link to main menu

---
+++ omod/src/main/java/org/openmrs/module/disa/extension/html/GutterListExt.java
@@ -0,0 +1,29 @@
+package org.openmrs.module.disa.extension.html;
+
+import org.openmrs.module.web.extension.LinkExt;
+
+public class GutterListExt extends LinkExt {
+
+    String url = "/module/disa/managelabresults.form";
+    String label = "Disa Interoperabilidade";
+
+    public String getLabel() {
+        return this.label;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    /**
+     * Returns the required privilege in order to see this section. Can be a
+     * comma delimited list of privileges. If the default empty string is
+     * returned, only an authenticated user is required
+     *
+     * @return Privilege string
+     */
+    public String getRequiredPrivilege() {
+        return "Pesquisar resultados no Disa Interoperabilidade";
+    }
+
+}
