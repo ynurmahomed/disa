2f7b84098a8af9381b9878bfd28e7435f206bffa
Yassin Nurmahomed <yassin.nurmahomed@fgh.org.mz>
2023-03-13 17:00:41 +0200

    Fix visits tab
    Disable favoring of path extensions

--- omod/src/main/java/org/openmrs/module/disa/web/config/DisaWebConfig.java
+++ omod/src/main/java/org/openmrs/module/disa/web/config/DisaWebConfig.java
@@ -1,9 +1,19 @@
 package org.openmrs.module.disa.web.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
-public class DisaWebConfig {
+public class DisaWebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        // In previous versions of spring mvc controllers would favor accept headers over path extensions.
+        // We need to support legacyui that makes requests with extensions that are not known to spring.
+        // Disable favoring of path extension so we can load the visits tab (requests are made with .list extension).
+        configurer.favorPathExtension(false);
+    }
 }
