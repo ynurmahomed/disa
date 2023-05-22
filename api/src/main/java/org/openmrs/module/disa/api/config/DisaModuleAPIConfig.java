ce2e339f2a52eb55acd22805f935c81b75c68024
Yassin Nurmahomed <yassin.nurmahomed@fgh.org.mz>
2023-05-22 09:14:09 +0200

    Use prototype scope
    * In order to get a new LabResultProcessor instance, preventing accumalation of processed count

--- api/src/main/java/org/openmrs/module/disa/api/config/DisaModuleAPIConfig.java
+++ api/src/main/java/org/openmrs/module/disa/api/config/DisaModuleAPIConfig.java
@@ -19,6 +19,7 @@ import org.openmrs.module.disa.api.sync.PatientNidLookup;
 import org.openmrs.module.disa.api.sync.ProviderLookup;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -44,6 +45,7 @@ public class DisaModuleAPIConfig {
     }
 
     @Bean
+    @Scope("prototype")
     public LabResultProcessor labResultHandler(
             DuplicateRequestIdLookup duplicateRequestIdLookup,
             PatientNidLookup patientNidLookup,
